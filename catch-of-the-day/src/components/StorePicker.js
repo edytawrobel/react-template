a03a2427ffddc20a0235406b1eb42c56d54a624f
Edyta Wrobel <ewrobel@thoughtworks.com>
2018-01-03 23:18:49 +0000

    use onSubmit={} event and this binding with the event; React Router <Redirect> component and imperative api -transitonTo to redirect to particular storeId page

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { getFunName } from '../helpers';
+
 class StorePicker extends React.Component {
+
+  // constructor() {
+  //   super(); // calss React.Component that inherits from
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(event) {
+    event.preventDefault();  //prevent form from submitting
+    const storeId = this.storeInput.value;     //grab the data submitted
+    this.context.router.transitionTo(`/store/${storeId}`);
+    //change the u rl from '/' to '/store/dataSubmitted'
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={ (e) => this.goToStore(e) }>
         {/* <p>Hello</p> */}
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store name" defaultValue={ getFunName() }/>
+        <input type="text" required placeholder="Store name" defaultValue={ getFunName() } ref={(input) => { this.storeInput = input }}/>
         <button type="submit">Visit Store</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
